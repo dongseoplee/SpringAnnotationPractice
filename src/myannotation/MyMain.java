1d0a915f18f9b00573ea0a73a22a13c151b65e40
Lee-DongSeop <76763417+Lee-DongSeop@users.noreply.github.com>
2022-01-12 15:06:12 +0900

    Second

--- src/myannotation/MyMain.java
+++ src/myannotation/MyMain.java
@@ -4,16 +4,16 @@ import java.lang.reflect.Method;
 
 public class MyMain {
     public static void main(String[] args) {
-        Method[] methodList=MyService.class.getMethods();
+        Method[] methodList = MyService.class.getMethods();
 
         for(Method m : methodList) {
             if(m.isAnnotationPresent(MyAnnotation.class)) {
                 System.out.println(m.getName());
-                MyAnnotation annotation=m.getDeclaredAnnotation(MyAnnotation.class);
+                MyAnnotation annotation = m.getDeclaredAnnotation(MyAnnotation.class);
 
-                String value=annotation.value();
-                int number=annotation.number();
-                for(int i=0;i<number;i++) {
+                String value = annotation.value();
+                int number = annotation.number();
+                for(int i = 0; i < number; i++) {
                     System.out.print(value);
                 }
                 System.out.println();
